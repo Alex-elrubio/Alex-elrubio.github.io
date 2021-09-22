3a2136cdd7fc609ddeb28792e38259e944273813
Alex-elrubio <larychev.port@gmail.com>
2021-09-22 20:59:41 +0300

    5

---
+++ homework5/5 rework.js
@@ -0,0 +1,39 @@
+let value;
+
+if (confirm("Ты хочешь пива??")) {
+    value = 'Is True';;
+} else{
+    value = 'Is False';;
+}
+
+const value2 = confirm("Ты хочешь пива??")
+ ? 'Is True' 
+ : 'Is False';
+
+console.log(value);
+console.log(value2);
+
+//
+
+let userAge = prompt('введите свой возраст?');
+let value10 = (userAge >= 18);
+let value20 = (userAge >= 30 );
+let value30 = (userAge < 18);
+
+if ((userAge >= 18) === value10) {
+    if ((userAge >= 30) === value20 ) {
+        value20 = 'Добро пожаловать!';
+    } else {
+        value10 = 'дарова атец ;)';
+    }
+} else ((userAge <18) === value30); {
+    value30 = "повзрослей малышь)";
+}
+
+const userAge = prompt('введите свой возраст?') 
+? value20('Добро пожаловать!') 
+: value("повзрослей малышь)");
+
+// 
+
+
